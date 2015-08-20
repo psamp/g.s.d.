2b761810fc5fbc93ff3d178944a4cc0d86df3180
Princess Sampson <ps@kingonomics.com>
2015-08-20 17:52:26 -0400

    closer to getting delete to work

--- app/js/main.js
+++ app/js/main.js
@@ -5,34 +5,23 @@
   // CREATE TASK CONSTRUCTOR
 
   var Todo = function (options) {
-  var args = options || {}
-
-  this.task = args.task;
-  this.status = 'Open';
-  this.timestamp = moment().format();
-
+    var args = options || {};
+    this.task = args.task;
+    this.timestamp = moment().format();
+    this.status = 'Open';
   };
-
-  // create empty array.
 
   var storageBin = [];
 
   // when addtask button is pressed, pull out the value of the input field. var task instance equals a new task with the name of what was input. push tasks to our storage; and finally append the text that was input to the #tasks ul.
 
   $('#addTask').on('submit', function (e) {
-
     e.preventDefault();
-
     var taskText = $('#taskText').val();
-
     var taskInstance = new Todo({task: taskText});
-
     storageBin.push(taskInstance);
-
     $('#tasks').append('<li class="open">' + taskText + '</li>');
-
     this.reset();
-
   });
 
   // on the click of an li inside of the #tasks ul: create a variable equal to the text of the li inside of #task. create a variable that looks within our storage for the task with the same title as ttask. redefine this task's status to closed.
@@ -40,11 +29,8 @@
   $('#tasks').on('click', 'li', function (e){
     e.preventDefault();
     var tTask = $(this).text();
-    
     var taskToEdit = _.find(storageBin, {task: tTask});
-
     taskToEdit.status = 'Closed';
-
     $(this).addClass('closed');
     $(this).removeClass('open');
   });
@@ -53,72 +39,22 @@
 
 
     $('#tasks').on('click', '.closed', function (e){
-    
-    e.preventDefault();
-
-    var tTask = $(this).text();
-    
-    var taskToEdit = _.find(storageBin, {task: tTask});
-
-    taskToEdit.status = 'Open';
-
-    $(this).removeClass('closed');
-    $(this).addClass('open');
-
+      e.preventDefault();
+      var tTask = $(this).text();
+      var taskToEdit = _.find(storageBin, {task: tTask});
+      taskToEdit.status = 'Open';
+      $(this).removeClass('closed');
+      $(this).addClass('open');
   });
 
 
-    $('#deleteButton').on('click', function (e){
-    
-    e.preventDefault();
-
-    var tClass = $('.closed');
-    
-    var closedTasks = _.find(storageBin, {status: 'Closed'});
-
-    console.log(closedTasks);
-
+    $('.deleteButton').on('click', function (e){
+      e.preventDefault();
+      var closed = function(todo) {
+        return todo.status === 'Closed';
+      };
+      var findClosed = storageBin.filter(closed);
+      console.log(findClosed);
   });
-
-  
-
-  // if (taskToEdit.status === 'Closed') {
-
-  //   // ...APPEND FINISHED TASKS TO THE DONE UL WITH A CSS CLASS THAT WILL PULL IN STYLES...
-
-  //   $('#tasks li').addClass('closed');
-
-  //   $('#tasksDone').append('<li class="closed">' + tTask + '</li>');
-  // }
-
-  // ON THE CLICK OF AN LI INSIDE OF THE #TASKSDONE UL:
-
-  // $('#tasksDone').on('click', 'li', function (e){
-  //   e.preventDefault();
-
-  //   var tTask = $(this).text();
-
-  //   // ...CREATE A VARIABLE THAT LOOKS WITHIN OUR STORAGE FOR THE TASK WITH THE SAME TITLE AS TTASK...
-    
-  //   var taskToEdit = _.find(storageBin, {task: tTask});
-
-  //   // ...REDEFINE OUR TASK'S STATUS TO OPEN...
-
-  //   // THE TASK IS CLOSED AND WE WANT TO MOVE IT BACK, SO...
-
-  //   if (taskToEdit.status === 'Closed') {
-  //     taskToEdit.status = 'Open';
-
-  //     $('#tasksDone').empty();
-
-  //     $('#tasksDone li').addClass('closed');
-
-  //     $('#tasks').append('<li class="open">' + tTask + '</li>');
-
-  //     $('#tasksDone').remove(tTask);
-  //   }
-
-  // });
-
 
 // }());
